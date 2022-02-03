cf560d0e85144d387d64c9da2ec367850550e7ab
Unelectrized <65368847+Unelectrized@users.noreply.github.com>
2022-02-03 13:43:27 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+const pages = {
+  {
+    "Robux";
+    "Robux is a currency that is used in Roblox."
+  }
+}
 
+const thingTemplate = document.querySelector("[data-thing-template]")
+const card = thingTemplate.content.cloneNode(true).children[0]
